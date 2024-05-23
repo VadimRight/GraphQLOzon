faff76ea1bc7fe2ec44369dec4d16f1cade3772e
VadimRight <vadimright@mail.ru>
2024-05-23 09:10:22 +0300

    Fit: server init func is up

--- api/api.go
+++ api/api.go
@@ -2,28 +2,36 @@ package api
 
 
 import (
-	"log"
-	"net/http"
-	"os"
-
+	"github.com/gin-gonic/gin"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/VadimRight/GraphQLOzon/graph"
+	"github.com/VadimRight/GraphQLOzon/bootstrap"
 )
 
-const defaultPort = "8080"
-
-func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
-
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
-
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
-
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+func InitServer(cfg *bootstrap.Config) {	
+	r := gin.Default()
+	r.POST("/query", graphqlHandler())
+	r.GET("/", playgroundHandler())
+	r.Run(cfg.Server.ServerPort)
 }
+
+func graphqlHandler() gin.HandlerFunc {
+	// NewExecutableSchema and Config are in the generated.go file
+	// Resolver is in the resolver.go file
+	h := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+// Defining the Playground handler
+func playgroundHandler() gin.HandlerFunc {
+	h := playground.Handler("GraphQL", "/query")
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"github.com/VadimRight/OzonTestTask/bootstrap"
+	"github.com/VadimRight/GraphQLOzon/bootstrap"
+	"github.com/VadimRight/GraphQLOzon/api"
 )
 
 func main() {
 	cfg := bootstrap.LoadConfig()
 	db := bootstrap.InitPostgresDatabase(cfg)
 	defer bootstrap.CloseDB(db)
+	api.InitServer(cfg)	
 }
