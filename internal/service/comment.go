af87c1b9cfa9843aa6f262a02d917e1fb110d7d6
VadimRight <vadimright@mail.ru>
2024-05-24 22:57:02 +0300

    Fit: service for comment

---
+++ internal/service/comment.go
@@ -0,0 +1,33 @@
+package service
+
+import (
+	
+	"github.com/VadimRight/GraphQLOzon/bootstrap"
+	"context"
+	"github.com/VadimRight/GraphQLOzon/graph/model"
+	"github.com/lib/pq"
+)
+
+type CommentService interface {
+	GetPostById(ctx context.Context, id string) (*model.CommentResponse, error)
+}
+
+type commentService struct {
+	storage bootstrap.Storage
+}
+
+func NewCommentService(storage bootstrap.Storage) CommentService {
+	return &commentService{storage: storage}
+}
+
+func (s *commentService) GetPostById(ctx context.Context, id string) (*model.CommentResponse, error) {
+	var comment model.CommentResponse
+	err := s.storage.DB.QueryRowContext(ctx, "SELECT id, username, password FROM users WHERE username=$1", id).Scan(&comment.ID, &comment.Comment)
+	if err != nil {
+		if postgresErr, ok := err.(*pq.Error); ok {
+    			return nil, postgresErr
+		}				
+		return nil, err
+	}
+	return &comment, nil
+}
