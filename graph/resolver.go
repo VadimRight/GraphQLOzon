2368b7f0060c19f4a7b183c3e82b8ca914af646c
VadimRight <vadimright@mail.ru>
2024-05-25 20:28:02 +0300

    Fit: getposts by user id is working now

--- graph/resolver.go
+++ graph/resolver.go
@@ -98,6 +98,15 @@ func (r *queryResolver) Posts(ctx context.Context) ([]*model.Post, error) {
 	return posts, nil
 }
 
+// Метод получения постов по ID пользователя
+func (r *queryResolver) PostsByUserID(ctx context.Context, userID string) ([]*model.Post, error) {
+	posts, err := r.PostService.GetPostsByUserID(ctx, userID)
+	if err != nil {
+		return nil, err
+	}
+	return posts, nil
+}
+
 // Метод получения поста по его ID
 func (r *queryResolver) Post(ctx context.Context, id string) (*model.Post, error) {
 	post, err := r.PostService.GetPostByID(ctx, id)
