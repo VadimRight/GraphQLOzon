ad1539394b51a71f68548f7ec8c14add5509dbe4
VadimRight <vadimright@mail.ru>
2024-05-25 13:36:37 +0300

    now memory in storage

--- storage/memory.go
+++ storage/memory.go
@@ -14,12 +14,22 @@ type InMemoryStorage struct {
 	mu       sync.RWMutex
 }
 
+// Функция возвращающая объект InMemoryStorage
 func NewInMemoryStorage() *InMemoryStorage {
 	return &InMemoryStorage{
 		users:    make(map[string]*model.User),
 		posts:    make(map[string]*model.Post),
 		comments: make(map[string]*model.CommentResponse),
 	}
+}
+
+func InitInMemoryStorage() *InMemoryStorage {
+	storage := NewInMemoryStorage()
+
+	// Создание начальных данных, если нужно
+	storage.mu.Lock()
+	defer storage.mu.Unlock()
+	return storage
 }
 
 func (s *InMemoryStorage) CreateUser(ctx context.Context, user *model.User) error {
