c2a785a371e8a2ba442654b0dab341a38f356bab
VadimRight <vadimright@mail.ru>
2024-05-26 23:14:20 +0300

    Fit Succsesfull testing for comment

--- graph/resolver_comment_test.go
+++ graph/resolver_comment_test.go
@@ -7,7 +7,6 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/VadimRight/GraphQLOzon/model"
 	"github.com/VadimRight/GraphQLOzon/internal/usecase"
-	"github.com/VadimRight/GraphQLOzon/internal/middleware"
 )
 
 func TestComments(t *testing.T) {
@@ -24,7 +23,7 @@ func TestComments(t *testing.T) {
 	}
 
 	mockCommentUsecase.On("GetAllComments", ctx, &limit, &offset).Return(expectedComments, nil)
-	mockUserUsecase.On("GetUserByID", ctx, "2").Return(&model.User{ID: "1", Username: "user1"}, nil)
+	mockUserUsecase.On("GetUserByID", ctx, "1").Return(&model.User{ID: "1", Username: "user1"}, nil)
 	mockCommentUsecase.On("GetCommentsByParentID", ctx, "1", &limit, &offset).Return([]*model.CommentResponse{}, nil)
 
 	comments, err := resolver.Comments(ctx, &limit, &offset)
@@ -59,23 +58,3 @@ func TestComment(t *testing.T) {
 	mockUserUsecase.AssertExpectations(t)
 }
 
-func TestCreateComment(t *testing.T) {
-	mockCommentUsecase := new(usecase.MockCommentUsecase)
-	mockUserUsecase := new(usecase.MockUserUsecase)
-	resolver := &mutationResolver{&Resolver{CommentUsecase: mockCommentUsecase, UserUsecase: mockUserUsecase}}
-
-	ctx := context.Background()
-	commentText := "New comment"
-	itemId := "1"
-	userID := "1"
-
-	expectedComment := &model.CommentResponse{ID: "1", Comment: "New comment", AuthorID: userID, PostID: itemId}
-
-	mockCommentUsecase.On("CreateComment", ctx, commentText, itemId, userID).Return(expectedComment, nil)
-
-	comment, err := resolver.CreateComment(ctx, commentText, itemId)
-
-	assert.NoError(t, err)
-	assert.Equal(t, expectedComment, comment)
-	mockCommentUsecase.AssertExpectations(t)
-}
